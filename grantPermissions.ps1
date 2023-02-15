f03d8688aaea2551c086cc71fb4ef414c0ff4b8f
Anthony Watherston <anwather@microsoft.com>
2023-02-15 14:05:38 +1100

    Initial commit

---
+++ grantPermissions.ps1
@@ -0,0 +1,29 @@
+$DestinationTenantId = "xxx" # Update with your tenant Id
+$MsiName = "add-user-to-group" # Name of system-assigned or user-assigned managed service identity. (System-assigned use same name as resource).
+
+$oPermissions = @(
+    "GroupMember.ReadWrite.All"
+    "User.Read.All"
+)
+
+$GraphAppId = "00000003-0000-0000-c000-000000000000" # Don't change this.
+
+$oMsi = Get-AzADServicePrincipal -Filter "displayName eq '$MsiName'"
+$oGraphSpn = Get-AzADServicePrincipal -Filter "appId eq '$GraphAppId'"
+
+$oAppRole = $oGraphSpn.AppRole | Where-Object { ($_.Value -in $oPermissions) -and ($_.AllowedMemberType -contains "Application") }
+
+Connect-MgGraph -TenantId $DestinationTenantId -ForceRefresh
+
+foreach ($AppRole in $oAppRole) {
+    $oAppRoleAssignment = @{
+        "PrincipalId" = $oMSI.Id
+        "ResourceId"  = $oGraphSpn.Id
+        "AppRoleId"   = $AppRole.Id
+    }
+  
+    New-MgServicePrincipalAppRoleAssignment `
+        -ServicePrincipalId $oAppRoleAssignment.PrincipalId `
+        -BodyParameter $oAppRoleAssignment `
+        -Verbose
+}
